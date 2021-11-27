0b234681f5e57cc4aa435fb82447bc9d0a9b892b
Laura Hidalgo Rivera <a20hirila@iesgrancapitan.org>
2021-11-27 11:37:29 +0100

    added player, (animations and movement)

---
+++ src/entity/Player.js
@@ -0,0 +1,48 @@
+import "phaser";
+
+export default class Player extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, key) {
+    super(scene, x, y, key);
+
+    // Add to scene
+    this.scene = scene;
+    this.scene.add.existing(this);
+
+    // Physics
+    this.scene.physics.world.enable(this);
+    this.setCollideWorldBounds(true);
+
+    // Animations
+    this.anims.create({
+      key: "right",
+      frames: [{ key: "player", frame: 0 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "idle",
+      frames: [{ key: "player", frame: 1 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "left",
+      frames: [{ key: "player", frame: 2 }],
+      frameRate: 20,
+    });
+  }
+
+  // Movement
+  update(cursors) {
+    if (cursors.left.isDown) {
+      this.setVelocityX(-200);
+      this.anims.play("left", true);
+    } else if (cursors.right.isDown) {
+      this.setVelocityX(200);
+      this.anims.play("right", true);
+    } else {
+      this.setVelocityX(0);
+      this.anims.play("idle");
+    }
+  }
+}
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,4 +1,5 @@
 import "phaser";
+import Player from "../entity/Player";
 
 export default class MainScene extends Phaser.Scene {
     constructor() {
@@ -8,10 +9,19 @@ export default class MainScene extends Phaser.Scene {
     preload() {
         this.load.image("background", "assets/background.png");
         this.load.image("floor", "assets/floor.png");
+        this.load.spritesheet("player", "assets/player.png", { frameWidth: 37, frameHeight: 64 });
     }
 
     create() {
         this.add.image(200, 300, "background");
         this.add.image(200, 590, "floor");
+
+        this.player = new Player(this, 200, 400, "player");
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        this.player.update(this.cursors);
     }
 }
