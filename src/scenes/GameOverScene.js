8979d65c6af5f9678f6ffdc2315b27bb3d120fed
Laura Hidalgo Rivera <a20hirila@iesgrancapitan.org>
2021-11-28 20:52:38 +0100

    added restart button to play again

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -1,21 +1,40 @@
 import "phaser";
 
 export default class GameOverScene extends Phaser.Scene {
-    constructor() {
-      super("GameOverScene");
-    }
+  constructor() {
+    super("GameOverScene");
+  }
 
-    init(score) {
-        this.score = score;
-    }
+  init(score) {
+    this.score = score;
+  }
 
-    preload() {
-        this.load.image("background2", "assets/background-2.png");
-    }
+  preload() {
+    this.load.image("background2", "assets/background-2.png");
+    this.load.image("button", "assets/button.png");
+  }
 
-    create() {
-        this.add.image(200, 300, "background2");
-        this.add.bitmapText(60, 210, "pixelFont", "GAME OVER", 50, 1).setDropShadow(5, 5, "#000", 1);
-        this.add.bitmapText(135, 280, "pixelFont", `SCORE\n${this.score}`, 40, 1).setDropShadow(5, 5, "#000", 1);
-    }
+  create() {
+    // Display game over
+    this.add.image(200, 300, "background2");
+    this.add
+      .bitmapText(60, 210, "pixelFont", "GAME OVER", 50, 1)
+      .setDropShadow(5, 5, "#000", 1);
+    this.add
+      .bitmapText(150, 260, "pixelFont", `SCORE\n${this.score}`, 30, 1)
+      .setDropShadow(5, 5, "#000", 1);
+
+    // Restart game button
+    this.restartButton = this.add.image(195, 350, "button").setInteractive();
+    this.restartButton.on("pointerdown", () => {
+        this.scene.start("MainScene");
+    })
+    this.restartButton.on("pointerover", () => {
+        this.restartButton.setScale(0.80);
+    })
+
+    this.restartButton.on("pointerout", () => {
+        this.restartButton.setScale(1);
+    })
+  }
 }
